8f43d97ac91355267ead1d75f71553a65b5e9ce7
satelital <satelital2020@protonmail.com>
2020-06-14 22:03:06 -0400

    hasta la 34

--- script.js
+++ script.js
@@ -30,7 +30,17 @@ function score() {
     q12,
     q13,
     q14,
-    q15
+    q15,
+    q16,
+    q17,
+    q18,
+    q19,
+    q20,
+    q21,
+    q22,
+    q23,
+    q24,
+    q25
   ];//ultima no lleva coma
 
   var answers = [
@@ -48,11 +58,30 @@ function score() {
     "b", //12
     "e", //13
     "d", //14
-    "d"  //15
+    "d", //15
+    "c", //16
+    "c", //17
+    "b", //18
+    "b", //19
+    "a", //20
+    "c", //21
+    "b", //22
+    "c", //23
+    "d", //24
+    "b", //25
+    "d", //26
+    "d", //27
+    "f", //28
+    "c", //29
+    "b", //30
+    "b", //31
+    "a", //32
+    "f", //33
+    "c"  //34
   ];//ultima no lleva coma
 
   var points = 0;
-  var total = 15;
+  var total = 34;
 
   for (var i = 0; i < total; i++) {
     if (questions[i] === answers[i]) {
